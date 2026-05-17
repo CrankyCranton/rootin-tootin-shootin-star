325243828bdf125e052114152fc2064b6e198502
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2026-05-17 15:56:49 -0400

    Second ending

--- level/level.gd
+++ level/level.gd
@@ -1,12 +1,15 @@
 class_name Level extends Node3D
 
 
-const LEAVE_DISTANCE := 200.0
-const FADE_START_DISTANCE := 150.0
+const LEAVE_DISTANCE := 150.0
+const FADE_START_DISTANCE := 100.0
+
+var game_ended := false
 
 @onready var fps: Label = $FPS
 @onready var clay: Clay = $Clay
 @onready var fade: ColorRect = $Fade
+@onready var ending_2_animation: VideoStreamPlayer = %Ending2Animation
 
 
 func _ready() -> void:
@@ -15,13 +18,20 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
-	fade.color.a = (minf(0.0, clay.position.length() - FADE_START_DISTANCE)
+	fade.color.a = (maxf(0.0, clay.position.length() - FADE_START_DISTANCE)
 			/ (LEAVE_DISTANCE - FADE_START_DISTANCE))
 	fps.text = str(Performance.get_monitor(Performance.TIME_FPS)) \
 			+ "\n" + str(clay.global_transform)
 
+	if clay.position.length() >= LEAVE_DISTANCE and not game_ended:
+		game_ended = true
+		ending_2_animation.play()
+		await ending_2_animation.finished
+		DialogueVars.end_game("Leaving Town", "Ending 2/1", Color.WHITE)
+
 
 func _on_dialogue_vars_game_ended(title_text: String, subtitle_text: String, color: Color) -> void:
+	game_ended = true
 	clay.set_physics_process(false)
 	clay.set_process_input(false)
 	var end_screen: EndScreen = preload("uid://cuiqlonvx3y5w").instantiate()
