81bc5e3249c5ec7f368e97d656ee75875d9e5be3
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2026-05-17 13:13:20 -0400

    Collision ramps

--- dialogue_vars.gd
+++ dialogue_vars.gd
@@ -4,4 +4,6 @@ extends Node
 var talked_to_seb := false
 var talked_to_howard1 := false
 var talked_to_howard2 := false
+var talked_to_howard3 := false
 var talked_to_apple := false
+var applehow := false
